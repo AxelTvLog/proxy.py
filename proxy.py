3b8544c66beff250d751ae9da1df842c4af10581
AxelTvLog <88273176+AxelTvLog@users.noreply.github.com>
2021-08-07 04:23:52 -0700

    proxy.py

--- proxy.py
+++ proxy.py
@@ -16,7 +16,7 @@ MSG = 'YAMETE KUDASAI'
 COR = '<font color="cyan">'
 FTAG = '</font>'
 DEFAULT_HOST = '0.0.0.0:22'
-RESPONSE = "HTTP/1.1 101 <h6><font color='#FE1EDB'>Mario Ozawo</font></h6>\r\nContent-Length: 1048576000000\r\n\r\n"
+RESPONSE = "HTTP/1.1 100 <font color='cyan'>Mario Ozawo</font>\r\nContent-Length: 1048576000000\r\n\r\n"
  
 class Server(threading.Thread):
     def __init__(self, host, port):
