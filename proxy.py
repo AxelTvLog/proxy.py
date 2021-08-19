3443bbe86bd4dbac7ea4ee2e6dd7ece5bceb9d3e
AxelTvLog <88273176+AxelTvLog@users.noreply.github.com>
2021-08-19 15:55:08 +0800

    proxy.py

--- proxy.py
+++ proxy.py
@@ -16,7 +16,7 @@ MSG = 'YAMETE KUDASAI'
 COR = '<font color="cyan">'
 FTAG = '</font>'
 DEFAULT_HOST = '0.0.0.0:22'
-RESPONSE = 'HTTP/1.1 101 SL_CDNOpenVPN_YNTKTS\r\n\r\n''
+RESPONSE = 'HTTP/1.1 101 <font color="cyan">Mario Ozawo</font>\r\n\r\n''
 #RESPONSE = 'HTTP/1.1 200 Hello_World!\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 Connection established\r\n\r\n'  # lint:ok
 
 class Server(threading.Thread):
