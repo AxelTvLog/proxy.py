a142acc424f2fb85298aea574bd03f0b8025a783
AxelTvLog <88273176+AxelTvLog@users.noreply.github.com>
2021-08-28 01:44:34 +0800

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -15,8 +15,8 @@ TIMEOUT = 60
 MSG = 'YAMETE KUDASAI'
 COR = '<font color="cyan">'
 FTAG = '</font>'
-DEFAULT_HOST = '0.0.0.0:22'
-RESPONSE = 'HTTP/1.1 101 Hello_World!\r\n\r\n'
+DEFAULT_HOST = '0.0.0.0:443'
+RESPONSE = 'HTTP/1.0 101 <font color="cyan">Isa Kang Tubol</font>\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):
